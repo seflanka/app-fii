38510d92ddb5b1488515c616f555ec46827e77d0
Guilherme de  Souza <91706877+seflanka@users.noreply.github.com>
2022-11-22 16:53:03 -0300

    update server

--- front-end/src/components/pages/Home.js
+++ front-end/src/components/pages/Home.js
@@ -5,8 +5,8 @@ function Home() {
     return (
         <section>
             <br />
-            <h1>Quarto deploy de teste</h1>
-            <h2>Agora vai</h2>
+            <h1>QUINTO deploy de teste</h1>
+
         </section>
     )
 }
